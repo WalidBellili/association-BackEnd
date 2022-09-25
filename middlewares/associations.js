727749ea17981ada75a2414764ae7cb30f58694f
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-25 14:42:17 +0200

    the method find helps me to compare my json slug and the slug of the params. if does'nt exist send me an error

--- middlewares/associations.js
+++ middlewares/associations.js
@@ -1,7 +1,18 @@
-const checkIfExists = (req, res, next) => {
-  console.log(req.params);
-};
+const associations = require("../associations.json");
 
+const checkIfExists = (req, res, next) => {
+  const { slug } = req.params;
+
+  const association = associations.find((association) => {
+    return association.slug === slug;
+  });
+
+  if (association) {
+    next();
+  } else {
+    res.status(404).json("NOT FOUND");
+  }
+};
 
 module.exports = {
   checkIfExists,
